6e65c4e2bf8b17cb02e443d070ec04d420750901
pawansikarwar0207 <pawansikarwar96@gmail.com>
2023-05-08 16:39:26 +0530

    add property details

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,5 @@
 class Property < ApplicationRecord
 	belongs_to :user
+
+	validates :name, :address, :price, :rooms, :bathrooms, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, presence: true
   
+  has_many :properties
+  
 end
